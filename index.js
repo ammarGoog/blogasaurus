ecafb25872f8b5108c274e179eb8a1ebf3b68fb0
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-12 09:57:32 -0700

    html profile complete and some javascript ideas

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("I'm working");
+
+//#leb #CLW #hiyyah #elbow
+// .secret .colorChange
+//https://stackoverflow.com/questions/38498246/how-to-make-an-html-element-appear-and-disappear-again-and-again-with-js
+
+/*
+const titles = document.querySelector(".colorChange");
+titles.addEventListener('hover', () => {
+  titles.style.color = 'red';
+  titles.style.font-size = 20px;
+})
+*/
